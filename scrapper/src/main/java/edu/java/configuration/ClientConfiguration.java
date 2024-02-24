07359a6b8522dc79de0d589dfc3546ad73365b0e
AlexSpeal <alexspeal@gmail.com>
2024-02-24 21:04:29 +0300

    scrapper first commit

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -1,10 +1,10 @@
 package edu.java.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class ClientConfiguration {
