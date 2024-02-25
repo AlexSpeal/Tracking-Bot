72f53ec9acb41731b926d812c49b10d3802cb826
AlexSpeal <alexspeal@gmail.com>
2024-02-25 19:59:46 +0300

    scrapper first commit

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -1,6 +1,5 @@
 package edu.java.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientConfiguration {
-    @Autowired
-    ApplicationConfig applicationConfig;
 
     @Bean("getGitHubClient")
     public WebClient getGitHubClient(@Value("${app.git-hub-base-url}") String baseUrl) {
@@ -23,5 +20,3 @@ public class ClientConfiguration {
         return WebClient.builder().baseUrl(baseUrl).build();
     }
 }
-
-
